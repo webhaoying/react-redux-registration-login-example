33eec66942122fc19e3edad0b3ceb88814cc9648
zhanghao <hao.zhang@roboterra.com>
2017-10-31 09:58:12 +0800

    add the devtools to present

--- src/_helpers/store.js
+++ src/_helpers/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware ,compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../_reducers';
@@ -7,8 +7,15 @@ const loggerMiddleware = createLogger();
 
 export const store = createStore(
     rootReducer,
-    applyMiddleware(
+    //  开启devtools的功能的话
+    compose(applyMiddleware(
         thunkMiddleware,
         loggerMiddleware
-    )
+    ),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+    //插件调试使用方式
+    // applyMiddleware(
+    //     thunkMiddleware,
+    //     loggerMiddleware
+    // )
 );
